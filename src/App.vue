cc4dc40b155048a83026b78ff3218237c31f96f1
ray <rayqian@outlook.com>
2017-05-10 14:03:33 +0800

    my first commit test

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+<!--     <firstComponent></firstComponent>
+    <secondComponent></secondComponent> -->
+    <ul>
+      <li><router-link to="/first">第一页</router-link></li>
+      <li><router-link to="/second">第二页</router-link></li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import firstComponent from './components/firstComponent.vue'
+// import secondComponent from './components/secondComponent.vue'
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/secondComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="secondComponent">
+    <h1>I am the second page.</h1>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      author: 'ray'
+    }
+  }
+}
+</script>
