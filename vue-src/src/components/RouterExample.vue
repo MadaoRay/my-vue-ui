4de290c9b3425f20ae9e89d4cece8f84780d1917
ray <rayqian@outlook.com>
2017-05-15 09:55:44 +0800

    add imgs & router example

---
+++ vue-src/src/components/RouterExample.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>this is router</div>
+</template>
+
+<script type="text/javascript">
+// export default {
+//   // name: 'routerLink',
+//   data () {
+//     return {
+//       msg: 'ray'
+//     }
+//   }
+// }
+export default {
+  data () {
+    return {
+      author: 'ray'
+    }
+  }
+}
+</script>
---
+++ vue-src/src/components/firstComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <div id='first'> -->
+  <div>
+  	<h1>I am the first component</h1>
+  	<div>wirrten by {{ author }}</div>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      author: 'ray'
+    }
+  }
+}
+</script>
